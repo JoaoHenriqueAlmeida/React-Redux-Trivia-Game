addcebc5384e0bb0e0e89eec7516b36750048885
Lombeira <glomenha@gmail.com>
2021-10-22 10:30:25 -0300

    Styling

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
+import '../../pages/login.css';
+
 export default class Button extends Component {
   render() {
     return (
       <Link to="/config">
-        <button data-testid="btn-settings" type="button">Config</button>
+        <button
+          className="btn-style"
+          data-testid="btn-settings"
+          type="button"
+        >
+          Config
+        </button>
       </Link>
     );
   }
